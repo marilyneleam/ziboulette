6f61e7511251a1b390a65f3e79f7c18c74f0c004
Anto <lignac.antony@live.fr>
2024-11-12 15:16:03 +0100

    fix ICustomProps

--- src/components/HighlightedCard.tsx
+++ src/components/HighlightedCard.tsx
@@ -1,42 +1,37 @@
 import { Box, Card, CardContent, CardMedia, Link } from "@mui/material";
-import {
-  isZibnewsPost,
-  IZibnewsPost,
-  IZibunityPost,
-} from "../types/IZibPost";
-import { CustomProps } from "../types/IComponents";
+import { isZibnewsPost, IZibnewsPost, IZibunityPost } from "../types/IZibPost";
+import { ICustomProps } from "../types/IComponents";
 
 const HighlightedCard = ({
   post,
   className,
-}: { post: IZibnewsPost | IZibunityPost } & CustomProps) => {
+}: { post: IZibnewsPost | IZibunityPost } & ICustomProps) => {
   return (
-	<Box>
-		      <Card component="article"
-          className={`${className || " "} bg-zib-light-gray rounded-lg overflow-hidden hover:shadow-md transition-shadow duration-300 w-full aspect-square`}
+    <Box>
+      <Card
+        component="article"
+        className={`${className || " "} bg-zib-light-gray rounded-lg overflow-hidden hover:shadow-md transition-shadow duration-300 w-full aspect-square`}
       >
-          <Link
-              href={`/zibnews/${post.slug}`}
-              underline="none"
-              className="block !text-black h-full"
-          >
-              {isZibnewsPost(post) && (
-                  <div className="h-[60%] relative">
-                      <CardMedia
-                          className="w-full h-full object-cover"
-                          image={post.image}
-                      />
-                  </div>
-              )}
-              <CardContent className="p-4 h-[40%] flex items-center justify-center text-center">
-                  <h1 className="text-lg font-semibold line-clamp-2">
-                      {post.title}
-                  </h1>
-              </CardContent>
-          </Link>
+        <Link
+          href={`/zibnews/${post.slug}`}
+          underline="none"
+          className="block !text-black h-full"
+        >
+          {isZibnewsPost(post) && (
+            <div className="h-[60%] relative">
+              <CardMedia
+                className="w-full h-full object-cover"
+                image={post.image}
+              />
+            </div>
+          )}
+          <CardContent className="p-4 h-[40%] flex items-center justify-center text-center">
+            <h1 className="text-lg font-semibold line-clamp-2">{post.title}</h1>
+          </CardContent>
+        </Link>
       </Card>
-	</Box>
-	  );
+    </Box>
+  );
 };
 
 export default HighlightedCard;

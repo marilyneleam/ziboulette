bbe9d45c2d920a92110bc0ca1643a5f20662052a
marilyne.leam <marilyne.leam@eemi.com>
2024-11-29 00:34:12 +0100

    add footerItems

--- src/types/IRoute.ts
+++ src/types/IRoute.ts
@@ -1,4 +1,4 @@
-import { Home, Newspaper, Users, Info, Heart } from "lucide-react";
+import { Home, Newspaper, Users, Info } from "lucide-react";
 
 export interface INavigationItem {
     name: string;
@@ -7,13 +7,19 @@ export interface INavigationItem {
     ariaLabel: string;
 }
 
+export interface IFooterItem {
+    name: string;
+    menu: { text: string; href: string }[];
+}
+
 export const navigationItems: INavigationItem[] = [
     { name: "Accueil", icon: Home, href: "/", ariaLabel: "Accueil" },
     {
         name: "Zibnews",
         icon: Newspaper,
         href: "/zibnews",
-        ariaLabel: "Consultez des nouvelles complètement improbables sur Zibnews",
+        ariaLabel:
+            "Consultez des nouvelles complètement improbables sur Zibnews",
     },
     {
         name: "Zibunity",
@@ -23,15 +29,47 @@ export const navigationItems: INavigationItem[] = [
             "Découvrez de nouvelles anecdotes de la communauté sur Zibunity",
     },
     {
-        name: "Favoris",
-        icon: Heart,
-        href: "/favoris",
-        ariaLabel: "Retrouvez vos Zibutity favoris",
+        name: "A propos",
+        icon: Info,
+        href: "/a-propos",
+        ariaLabel: "En savoir plus sur Ziboulette et son équipe",
+    },
+];
+
+export const footerItems: IFooterItem[] = [
+    {
+        name: "Ziboulette",
+        menu: [
+            { text: "28 place de la Bourse<br>75002 Paris", href: "/contact" },
+            { text: "01 23 45 67 89", href: "tel:0123456789" },
+        ],
+    },
+	{
+		name: "Navigation",
+		menu: [
+			{ text: "Accueil", href: "/" },
+			{ text: "Zibnews", href: "/zibnews" },
+			{ text: "Zibunity", href: "/zibunity" },
+			{ text: "Qui sommes-nous", href: "/qui-sommes-nous" },
+		],
+	},
+    {
+        name: "Liens utiles",
+        menu: [
+            { text: "Plan du site", href: "/plan-site" },
+            { text: "FAQ", href: "/faq" },
+            { text: "Retour en haut de page", href: "#top" },
+            { text: "Sections principales", href: "/sections" },
+        ],
     },
     {
-        name: "Qui sommes-nous",
-        icon: Info,
-        href: "/qui-sommes-nous",
-        ariaLabel: "En savoir plus sur Ziboulette et son équipe",
+        name: "Informations légales",
+        menu: [
+            { text: "Mentions légales", href: "/mentions-legales" },
+            { text: "Politique de confidentialité", href: "/confidentialite" },
+            { text: "CGU", href: "/cgu" },
+            { text: "Politique des cookies", href: "/cookies" },
+            { text: "Copyright et droit d'auteur", href: "/copyright" },
+        ],
     },
 ];

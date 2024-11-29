a6c09470a9e4b43578208aeefb062341f183ddf3
marilyne.leam <marilyne.leam@eemi.com>
2024-11-29 16:11:32 +0100

    add posttype

--- src/types/IZibPost.ts
+++ src/types/IZibPost.ts
@@ -1,24 +1,33 @@
-export interface IZibnewsPost {
+interface BasePost {
     id: number;
     title: string;
     content: string;
-    shortContent: string;
     slug: string;
     image: string;
     date: string;
     category: string;
 }
 
-export interface IZibunityPost {
-    id: number;
-    title: string;
-    content: string;
-    slug: string;
+export interface IZibnewsPost extends BasePost {
+    shortContent: string;
+    tags?: string[];
+    postType: "zibnews";
+}
+
+export interface IZibunityPost extends BasePost {
+    tags?: string[];
+    postType: "zibunity";
+}
+
+interface RawZibunityPost extends Omit<IZibunityPost, "postType"> {
 }
 
 type TPost = IZibnewsPost | IZibunityPost;
 
-
 export function isZibnewsPost(post: TPost): post is IZibnewsPost {
-    return (post as IZibnewsPost).image !== undefined;
+    return post.postType === "zibnews";
+}
+
+export function isZibunityPost(post: TPost): post is IZibunityPost {
+    return post.postType === "zibunity";
 }
